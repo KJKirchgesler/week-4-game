c33b8bc110985bf76ffeb088225f314ba24f12f7
KRISTEN KIRCHGESLER <kristen@KRISTENs-MacBook-Pro.local>
2018-02-22 17:05:02 -0500

    Added code to click crystals and a test counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,16 @@
-alert("Hi!");
+$(document).ready(function() {
+
+
+ //Created a test counter to track the user's total.
+ var counter = 0;
+ $(".crystal").on("click", function() {
+
+// This time we increase the counter by *10* each time the user clicks.
+ counter += 10;
+
+// We then output the new counter value each time the crystal is clicked.
+alert("Your new score is: " + counter);
+
+});	
+
+});	
