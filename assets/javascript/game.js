b71e0d093a8d8e1fa9d61406e263bb088b179aa0
KRISTEN KIRCHGESLER <kristen@KRISTENs-MacBook-Pro.local>
2018-02-23 10:00:28 -0500

    added initial code to compare target number and player's score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,37 @@
 $(document).ready(function() {
+});	
 
+ 
+// Here we establish the "targetNumber" (set to 50 in this example).
+var targetNumber = 50;
 
- //Created a test counter to track the user's total.
- var counter = 0;
- $(".crystal").on("click", function() {
+// Here we set the "number-to-guess" header to match the "targetNumber".
+$("#number-to-guess").text(targetNumber);
 
-// This time we increase the counter by *10* each time the user clicks.
- counter += 10;
+//Created a test counter to track the user's total.
+var counter = 0;
+$(".crystal").on("click", function() {
 
-// We then output the new counter value each time the crystal is clicked.
-alert("Your new score is: " + counter);
+	// This time we increase the counter by *10* each time the user clicks.
+ 	counter += 10;
+
+	// We then output the new counter value each time the crystal is clicked.
+	alert("New score: " + counter);
+
+    // Checking if the click counter matches the targetNumber.
+    if (counter === targetNumber) {
+
+      // If the numbers match the player wins.
+      alert("You win!");
+    }
+
+	// If the user's counter  xceeds the targetNumber...
+    else if (counter >= targetNumber) {
+
+      // The player loses.
+      alert("You lose!!");
+    }
 
 });	
 
-});	
+
