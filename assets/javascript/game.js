b119e8a73ccf20297547683b24752f289213b190
KRISTEN KIRCHGESLER <kristen@KRISTENs-MacBook-Pro.local>
2018-02-23 10:20:26 -0500

    added test code to randomize crystal values and increment to counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,15 +5,22 @@ $(document).ready(function() {
 // Here we establish the "targetNumber" (set to 50 in this example).
 var targetNumber = 50;
 
-// Here we set the "number-to-guess" header to match the "targetNumber".
-$("#number-to-guess").text(targetNumber);
+// Here we set the "number-to-match" header to match the "targetNumber".
+$("#number-to-match").text(targetNumber);
 
 //Created a test counter to track the user's total.
 var counter = 0;
+
+// Here we created code that selects a number either 10 or 11.
+// We'll set this number at the start of the "game".
+var numberOptions = [10, 11];
+var increment = numberOptions[Math.round(Math.random())];
+
+
 $(".crystal").on("click", function() {
 
 	// This time we increase the counter by *10* each time the user clicks.
- 	counter += 10;
+ 	counter += increment;
 
 	// We then output the new counter value each time the crystal is clicked.
 	alert("New score: " + counter);
