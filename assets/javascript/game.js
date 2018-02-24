f3f345986ee5f870be0e8338521ffdf710692391
KRISTEN KIRCHGESLER <kristen@KRISTENs-MBP.attlocal.net>
2018-02-24 01:39:23 -0500

    added code to click crystals and show random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,19 +12,27 @@
 
 	//Creating random numbers for each crystal
 	var randomCrystalValue = Math.floor(Math.random() * 11) + 1;
-	console.log(randomCrystalValue);
+	//console.log(randomCrystalValue);
 	
+	//Creating the div for the crystals
 	var crystal = $("<div>");
+		
+		//Assigning a class and random data values to each crystal
 		crystal.attr({
 			"class": "crystal",
 			"data-random" : randomCrystalValue
 			});
 
+	//Adding crystals images to the DOM
 	$(".crystals").append(crystal);
-		
-
 
 }
+
+
+	$(".crystal").on('click', function () {
+		console.log($(this).attr("data-random"));
+	
+});
 
 
 
