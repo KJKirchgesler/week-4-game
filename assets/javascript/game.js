f77c0bfb1495862c7135044d0243657c13c2fded
KRISTEN KIRCHGESLER <kristen@KRISTENs-MBP.attlocal.net>
2018-02-24 00:55:49 -0500

    added code for random number between 19 and 120

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,23 @@
-for(var i = 0; i < 4; i++) {
 
-	var random = Math.floor(Math.random() * 11) + 1;
-	console.log(random);
+	//Generating a new random number between 19 and 120
+	randomNumber = Math.floor(Math.random() * 101 ) + 19; 
+
+	//Adding the random number to the DOM
+	$("#number-to-match").html("Random Number: " + randomNumber);
+
+	//Looping 4 times to create random numbers for crystals
+	for(var i = 0; i < 4; i++) {
+
+
+
+	//Creating random numbers for each crystal
+	var randomCrystalValue = Math.floor(Math.random() * 11) + 1;
+	console.log(randomCrystalValue);
 	
 	var crystal = $("<div>");
 		crystal.attr({
 			"class": "crystal",
-			"data-random" : random
+			"data-random" : randomCrystalValue
 			});
 
 	$(".crystals").append(crystal);
