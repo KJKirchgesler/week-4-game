8a69917e8d6b0da981079e073f0ef9155bb64868
KRISTEN KIRCHGESLER <kristen@KRISTENs-MBP.attlocal.net>
2018-02-24 00:16:24 -0500

    added random values to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,13 @@
 for(var i = 0; i < 4; i++) {
+
+	var random = Math.floor(Math.random() * 11) + 1;
+	console.log(random);
 	
 	var crystal = $("<div>");
-		crystal.attr("class", "crystal");
+		crystal.attr({
+			"class": "crystal",
+			"data-random" : random
+			});
 
 	$(".crystals").append(crystal);
 		
