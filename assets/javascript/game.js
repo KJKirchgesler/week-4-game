dbd4faaf0bfc6b1331cfecd8c33e4931d6833daf
KRISTEN KIRCHGESLER <kristen@KRISTENs-MBP.attlocal.net>
2018-02-24 00:00:42 -0500

    redoing code in game.js to create dynamic crystals. Added css.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,17 @@
-$(document).ready(function() {
-});	
+for(var i = 0; i < 4; i++) {
+	
+	var crystal = $("<div>");
+		crystal.attr("class", "crystal");
 
- 
-// Here we establish the "targetNumber" (set to 50 in this example).
-var targetNumber = 50;
-
-// Here we set the "number-to-match" header to match the "targetNumber".
-$("#number-to-match").text(targetNumber);
-
-//Created a test counter to track the user's total.
-var counter = 0;
-
-// Here we created code that selects a number either 10 or 11.
-// We'll set this number at the start of the "game".
-var numberOptions = [10, 11];
-var increment = numberOptions[Math.round(Math.random())];
+	$(".crystals").append(crystal);
+		
 
 
-$(".crystal").on("click", function() {
+}
 
-	// This time we increase the counter by *10* each time the user clicks.
- 	counter += increment;
 
-	// We then output the new counter value each time the crystal is clicked.
-	alert("New score: " + counter);
 
-    // Checking if the click counter matches the targetNumber.
-    if (counter === targetNumber) {
 
-      // If the numbers match the player wins.
-      alert("You win!");
-    }
 
-	// If the user's counter  xceeds the targetNumber...
-    else if (counter >= targetNumber) {
-
-      // The player loses.
-      alert("You lose!!");
-    }
-
-});	
 
 
